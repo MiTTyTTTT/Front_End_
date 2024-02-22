24af99615ccd48edf386bd9830a98812a95e925c
MiTTyTTTT <s6406021622044@email.kmutnb.ac.th>
2024-02-22 16:06:57 +0700

    depl

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -12,8 +12,8 @@ const path = require('path');
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
-//const base_url = "http://localhost:3000";
-const base_url = "node58148-noderest2.proen.app.ruk-com.cloud";
+const base_url = "http://localhost:3000";
+//const base_url = "node58148-noderest2.proen.app.ruk-com.cloud";
 
 // Set the template engine
 app.set("views",path.join(__dirname, "/public/views"));
